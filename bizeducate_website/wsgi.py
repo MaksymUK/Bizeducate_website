53ea152dad50332d3fa4e4aa2eafe97632ac3385
MaksymUK <clear>
2024-01-19 22:11:25 +0000

    Refactor bizeducate_website directory from capital to lower letter

--- bizeducate_website/wsgi.py
+++ bizeducate_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Bizeducate_website project.
+WSGI config for bizeducate_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bizeducate_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bizeducate_website.settings')
 
 application = get_wsgi_application()
